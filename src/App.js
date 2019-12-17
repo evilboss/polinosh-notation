3afc8b37fd0950c5e28542218e992a89d9dd2f84
Jr Reyes <jr.evilboss@gmail.com>
2019-12-17 23:25:46 +0800

    add ui components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Button } from 'antd';
+import {Button, InputNumber, Select} from 'antd';
 
 import './App.css';
+const { Option } = Select;
 
 const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
+	<div className="App">
+		<InputNumber size="large" min={1} max={100000}/>
+		<Select
+			showSearch
+			style={{width: 200}}
+			placeholder="Select Operation"
+			optionFilterProp="children"
+			filterOption={(input, option) =>
+				option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+			}
+		>
+			<Option value="jack">Jack</Option>
+			<Option value="lucy">Lucy</Option>
+			<Option value="tom">Tom</Option>
+		</Select>,
+
+		<Button type="primary">Button</Button>
+	</div>
 
 );
 
