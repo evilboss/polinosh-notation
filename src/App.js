82889eefcacba9747ba7b22a67a87887cbb0a03e
Jr Reyes <jr.evilboss@gmail.com>
2019-12-17 23:46:18 +0800

    refactor evaluator to result compnent

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
 import React from 'react';
 import {Button, InputNumber, Select} from 'antd';
-
+import ResultComponent from './ResultComponent';
 import './App.css';
 const { Option } = Select;
-var operators = {
-	"+": [function(a, b){ return b+a }, 2],
-	"-": [function(a, b){ return b-a }, 2],
-	"*": [function(a, b){ return b*a }, 2],
-	"/": [function(a, b){ return b/a }, 2],
-	"^": [function(a, b){ return Math.pow(b,a) }, 2],
-	"sin": [function(a){ return Math.sin(a) }, 1],
-	"cos": [function(a){ return Math.cos(a) }, 1],
-	"tan": [function(a){ return Math.tan(a) }, 1],
-	"floor": [function(a){ return a|0 }, 1],
-	"ceil": [function(a){ return Math.ceil(a) }, 1],
-	"round": [function(a){ return Math.round(a) }, 1],
-	"ln": [function(a){ return Math.log(a) }, 1],
-	"sqrt": [function(a){ return Math.sqrt(a) }, 1],
-	"e": [function(){ return Math.E }, 0],
-	"pi": [function(){ return Math.PI }, 0]
-}
-function evalStr(str, mul){
-	var stack = [];
-	str = str.trim();
-	if(str.length == 0) return 0;
-	if(mul) str = str.toLowerCase().split(" ");
-	for(var i = 0; i < str.length; i++){
-		if(str[i] in operators){
-			var numArg = operators[str[i]][1];
-			if(stack.length < numArg) throw "Missing operant";
-			stack.push(operators[str[i]][0].apply(null, (function(){
-				var arr = [];
-				for(var j = 0; j < numArg; j++){
-					arr.push(stack.pop());
-				}
-				return arr;
-			})()));
-		}else{
-			if(!/^[0-9.]+$/.test(str[i])) throw "Unknown operator \""+str[i]+"\"";
-			stack.push(+str[i]);
-		}
-	}
-	if(stack.length != 1) throw "Uncaught extra operant";
-	return stack[0];
-}
+
 const App = () => (
 	<div className="App">
 		<InputNumber size="large" min={1} max={100000}/>
@@ -60,6 +20,7 @@ const App = () => (
 			<Option value="lucy">Lucy</Option>
 			<Option value="tom">Tom</Option>
 		</Select>,
+		<ResultComponent/>
 
 		<Button type="primary">Button</Button>
 	</div>
